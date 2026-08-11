2c662bc170b0941c4b034d0d6dac21a2f268d40e
xianghang zeng <62739219+zengxianghang@users.noreply.github.com>
2026-08-11 11:39:57 +0800

    Export RANGE parser for issue #3

--- python/gnss_parser/novatel/__init__.py
+++ python/gnss_parser/novatel/__init__.py
@@ -8,15 +8,31 @@ from .ascii import (
     parse_ascii_line,
 )
 from .psrvel import PsrvelRecord, iter_psrvel, parse_psrvel_line, read_psrvel
+from .range import (
+    RangeObservation,
+    RangeRecord,
+    TrackingStatus,
+    decode_tracking_status,
+    iter_range,
+    parse_range_line,
+    read_range,
+)
 
 __all__ = [
     "NovatelAsciiHeader",
     "NovatelAsciiMessage",
     "NovatelAsciiParseError",
     "PsrvelRecord",
+    "RangeObservation",
+    "RangeRecord",
+    "TrackingStatus",
+    "decode_tracking_status",
     "iter_psrvel",
+    "iter_range",
     "novatel_crc32",
     "parse_ascii_line",
     "parse_psrvel_line",
+    "parse_range_line",
     "read_psrvel",
+    "read_range",
 ]
