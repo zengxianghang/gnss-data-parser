cf60543daf5f7d054c56c3bf31b45e7879cbc8c7
xianghang zeng <62739219+zengxianghang@users.noreply.github.com>
2026-08-11 13:25:00 +0800

    Export mixed log APIs and align package version

--- python/gnss_parser/__init__.py
+++ python/gnss_parser/__init__.py
@@ -1,6 +1,26 @@
 """Shared GNSS parser package."""
 
 from .common.gpst import GPS_SECONDS_PER_WEEK, GpsTime, normalize_gpst
+from .mixed import (
+    SUPPORTED_MESSAGE_KEYS,
+    GnssLogEvent,
+    GnssLogResult,
+    GnssLogStats,
+    iter_gnss_log,
+    normalize_message_selection,
+    read_gnss_log,
+)
 
-__all__ = ["GPS_SECONDS_PER_WEEK", "GpsTime", "normalize_gpst"]
-__version__ = "0.1.0"
+__all__ = [
+    "GPS_SECONDS_PER_WEEK",
+    "GpsTime",
+    "GnssLogEvent",
+    "GnssLogResult",
+    "GnssLogStats",
+    "SUPPORTED_MESSAGE_KEYS",
+    "iter_gnss_log",
+    "normalize_gpst",
+    "normalize_message_selection",
+    "read_gnss_log",
+]
+__version__ = "0.8.0"
